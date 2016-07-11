1e665287aafddad390ffb84ab735556fe51cdded
yamadapc <tacla.yamada@gmail.com>
2016-07-11 19:48:16 -0300

    Big workspace update

--- examples/ButtonExample.hs
+++ examples/ButtonExample.hs
@@ -1,20 +1,21 @@
-import Control.Concurrent
-import Graphics.LibUI
-import Foreign.C
-import Foreign.Ptr
+import           Control.Concurrent
+import           Control.Exception
+import           Foreign.C
+import           Foreign.Ptr
+import           Graphics.LibUI
 
 main :: IO ()
-main = do
-    title <- newCString "haskell-libui - Control Gallery"
-    win <- c_uiNewWindow title 640 480 1
-    btn <- c_uiNewButton =<< newCString "Here I am"
-    mcount <- newMVar 0
-    let onClick b _ = do
-            count <- modifyMVar mcount (\c -> return (c + 1, c + 1)) :: IO Int
-            c_uiButtonSetText b =<< newCString (show count)
-    cb <- c_wrap2 onClick
-    c_uiButtonOnClicked btn cb nullPtr
-    c_uiWindowSetChild win btn
-    c_uiWindowSetMargined win 10
-    c_uiControlShow win
-    c_uiMain
+main = stuff
+    -- title <- newCString "haskell-libui - Control Gallery"
+    -- win <- c_uiNewWindow title 640 480 1
+    -- btn <- c_uiNewButton =<< newCString "Here I am"
+    -- mcount <- newMVar 0
+    -- let onClick b _ = do
+    --         count <- modifyMVar mcount (\c -> return (c + 1, c + 1)) :: IO Int
+    --         c_uiButtonSetText b =<< newCString (show count)
+    -- cb <- c_wrap2 onClick
+    -- c_uiButtonOnClicked btn cb nullPtr
+    -- c_uiWindowSetChild win btn
+    -- c_uiWindowSetMargined win 10
+    -- c_uiControlShow win
+    -- c_uiMain
--- src/Graphics/LibUI.hs
+++ src/Graphics/LibUI.hs
@@ -1,18 +1,321 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE InterruptibleFFI         #-}
+{-# LANGUAGE OverloadedStrings        #-}
+{-# LANGUAGE RecordWildCards          #-}
+{-# LANGUAGE ScopedTypeVariables      #-}
+{-# LANGUAGE TupleSections            #-}
 module Graphics.LibUI
   where
 
-import           Foreign
+import           Control.Applicative
+import           Control.Concurrent
+import           Control.Concurrent.Async
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans
+import           Data.String
+import           Foreign                  hiding (void)
+import qualified Foreign                  as Foreign
 import           Foreign.C
+
+-- data UIControl = UIControlWindow UIWindow
+--                | UIControlButton UIButton
+--                | UIControlBox UIBox
+--                | UIControlCheckbox UICheckbox
+--                | UIControlEntry UIEntry
+--                | UIControlLabel UILabel
+--                | UIControlTab UITab
+--                | UIControlGroup UIGroup
+--                | UIControlSpinbox UISpinbox
+--                | UIControlSlider UISlider
+--                | UIControlProgressBar UIProgressBar
+--                | UIControlSeparator UISeparator
+--                | UIControlCombobox UICombobox
+--                | UIControlEditableCombobox UIEditableCombobox
+--                | UIControlRadioButtons UIRadioButtons
+--                | UIControlMultlineEntry UIMultlineEntry
+--                | UIControlMenuItem UIMenuItem
+--                | UIControlMenu UIMenu
+
+data Term a = Term { runTerm :: IO CUIControl
+                   }
+
+term :: ToCUIControl t => t -> Term t
+term t = Term (toCUIControl t)
+
+data UI a = UI { runUI :: IO (a, [CUIControl])
+               }
+
+instance Functor UI where
+    f `fmap` ui = UI $ runUI ui >>= \(a, c) -> return (f a, c)
+
+instance Monoid a => Monoid (UI a) where
+    mempty = UI (return (mempty, []))
+    ui1 `mappend` ui2 = UI $ do
+        (a, cui1) <- runUI ui1
+        (b, cui2) <- runUI ui2
+        return (a `mappend` b, cui1 ++ cui2)
+
+instance Applicative UI where
+    pure = return
+    (<*>) = ap
+
+instance Monad UI where
+    return x = UI (return (x, []))
+    ui >>= a = UI $ do
+        (r, cui1) <- runUI ui
+        (r', cui2) <- runUI $ a r
+        return (r', cui1 ++ cui2)
+
+wrap toCUI = UI $ do
+    cui <- toCUIControl toCUI
+    return ((), [cui])
+
+runUILoop :: UI a -> IO ()
+runUILoop ui = run >> putStrLn "I'm still here"
+  where
+    run = do
+        alloca $ \ptr -> do
+            poke ptr (CSize (fromIntegral (sizeOf (CSize 0))))
+            c_uiInit ptr
+        (_, cs) <- runUI ui
+        mapM_ c_uiControlShow cs
+        forM_ cs $ \c -> do
+            cb <- c_wrap2 (const (const (void abort)))
+            c_uiWindowOnClosing c cb nullPtr
+        cb <- c_wrap1I (const abort)
+        c_uiOnShouldQuit cb nullPtr
+        a <- async $ c_uiMain
+        wait a `catch` \UserInterrupt -> print "interrupt" >> void abort
+    abort = do
+        c_uiUninit
+        return 0
+
+window title width height hasMenubar child = UI $ do
+    c <- toCUIControl $ UIWindow title width height hasMenubar child
+    return ((), [c])
+
+button title = wrap (UIButton title)
+
+vbox ui = UI $ do
+    (x, cs) <- runUI ui
+    c <- toCUIControl $ UIVerticalBox 0 (map (False,) cs)
+    return (x, [c])
+
+menu name items = UI $ do
+    c <- toCUIControl $ UIMenu name items
+    c `seq` (return ())
+    return ((), [])
+
+stuff :: IO ()
+stuff = runUILoop ui
+  where
+    ui :: UI ()
+    ui = do
+        menu "File" [ "Open"
+                    , "Save"
+                    , UIMenuItemQuit
+                    ]
+        window "Stuff" 640 300 True $ vbox $ do
+            button "Click me 1"
+            button "Click me 2"
+        -- button "Click me"
+
+class ToCUIControl c where
+    toCUIControl :: c -> IO CUIControl
+
+instance ToCUIControl UIWindow where
+    toCUIControl UIWindow{..} = do
+        w <- join $ c_uiNewWindow
+            <$> newCString uiWindowTitle
+            <*> return (fromIntegral uiWindowWidth)
+            <*> return (fromIntegral uiWindowHeight)
+            <*> return (if uiWindowHasMenubar then 1 else 0)
+        (_, cs) <- runUI uiWindowChild
+        case cs of
+            (c:_) -> c_uiWindowSetChild w c
+            _ -> return ()
+        return w
+
+instance ToCUIControl UIButton where
+    toCUIControl UIButton{..} =
+        join $ c_uiNewButton
+            <$> newCString uiButtonText
+
+instance ToCUIControl UIBox where
+    toCUIControl UIHorizontalBox{..} = do
+        b <- c_uiNewHorizontalBox
+        c_uiBoxSetPadded b (fromIntegral uiBoxPadding)
+        mapM_ (uncurry (c_uiBoxAppend b))
+            (map (\(b, c) -> (c, if b then 1 else 0)) uiBoxChildren)
+        return b
+    toCUIControl UIVerticalBox{..} = do
+        b <- c_uiNewVerticalBox
+        c_uiBoxSetPadded b (fromIntegral uiBoxPadding)
+        mapM_ (uncurry (c_uiBoxAppend b))
+            (map (\(b, c) -> (c, if b then 1 else 0)) uiBoxChildren)
+        return b
+
+instance ToCUIControl UICheckbox where
+    toCUIControl UICheckbox{..} = do
+        c <- c_uiNewCheckbox =<< newCString uiCheckboxText
+        c_uiCheckboxSetChecked c (if uiCheckboxChecked then 1 else 0)
+        return c
+
+instance ToCUIControl UIEntry where
+    toCUIControl UIEntry{..} = do
+        e <- c_uiNewEntry
+        c_uiEntrySetText e =<< newCString uiEntryText
+        c_uiEntrySetReadOnly e (if uiEntryReadOnly then 1 else 0)
+        return e
+
+instance ToCUIControl UILabel where
+    toCUIControl UILabel{..} = do
+        c_uiNewLabel =<< newCString uiLabelText
+
+instance ToCUIControl UITab where
+    toCUIControl UITab{..} = do
+        t <- c_uiNewTab
+        c_uiTabSetMargined t (fromIntegral uiTabMargin)
+        mapM_ (uncurry (c_uiTabAppend t)) =<< (forM uiTabChildren $ \(n, c) -> do
+            n' <- newCString n
+            return (n', c))
+        return t
+
+instance ToCUIControl UIGroup where
+    toCUIControl UIGroup{..} = do
+        g <- c_uiNewGroup =<< newCString uiGroupTitle
+        c_uiGroupSetMargined g (fromIntegral uiGroupMargin)
+        c_uiGroupSetChild g uiGroupChild
+        return g
+
+instance ToCUIControl UIMenu where
+    toCUIControl UIMenu{..} = do
+        m <- c_uiNewMenu =<< newCString uiMenuName
+        forM_ uiMenuItems $ \item ->
+            appendMenuItem m item
+        print "Menu created"
+        return m
+
+appendMenuItem m UIMenuItem{..} = do
+    ctext <- newCString uiMenuItemText
+    c_uiMenuAppendItem m ctext
+appendMenuItem m UIMenuItemQuit = do
+    c_uiMenuAppendQuitItem m
+
+-- getCUIControl _ = undefined
+
+data UIWindow = UIWindow { uiWindowTitle      :: String
+                         , uiWindowWidth      :: Int
+                         , uiWindowHeight     :: Int
+                         , uiWindowHasMenubar :: Bool
+                         , uiWindowChild      :: UI ()
+                         }
+
+data UIButton = UIButton { uiButtonText :: String
+                         }
+
+data UIBox = UIHorizontalBox { uiBoxPadding  :: Int
+                             , uiBoxChildren :: [(Bool, CUIControl)]
+                             }
+           | UIVerticalBox { uiBoxPadding  :: Int
+                           , uiBoxChildren :: [(Bool, CUIControl)]
+                           }
+
+data UICheckbox = UICheckbox { uiCheckboxChecked :: Bool
+                             , uiCheckboxText    :: String
+                             }
+
+data UIEntry = UIEntry { uiEntryReadOnly :: Bool
+                       , uiEntryText     :: String
+                       }
+             -- TODO - | UIPasswordEntry {
+             --                   }
+
+data UILabel = UILabel { uiLabelText :: String
+                       }
+
+data UITab = UITab { uiTabMargin   :: Int
+                   , uiTabChildren :: [(String, CUIControl)]
+                   }
+
+data UIGroup = UIGroup { uiGroupTitle  :: String
+                       , uiGroupMargin :: Int
+                       , uiGroupChild  :: CUIControl
+                       }
+
+data UISpinbox = UISpinbox { uiSpinboxValue :: Int
+                           , uiSpinboxMin   :: Int
+                           , uiSpinboxMax   :: Int
+                           }
+
+data UISlider = UISlider { uiSliderValue :: Int
+                         , uiSliderMin   :: Int
+                         , uiSliderMax   :: Int
+                         }
+
+data UIProgressBar = UIProgressbar { uiProgressbarValue :: Int
+                                   }
+
+data UISeparator = UIHorizontalSeparator
+                 | UIVerticalSeparator
+
+data UICombobox = UICombobox { uiComboboxSelected :: Bool
+                             }
+
+data UIEditableCombobox = UIEditableCombobox { uiEditableComboboxText :: String
+                                             }
+
+data UIRadioButtons = UIRadioButtons { uiRadioButtonsSelected :: Int
+                                     }
+
+data UIMultlineEntry = UIMultilineEntry { uiMultilineEntryText     :: String
+                                        , uiMultilineEntryReadOnly :: Bool
+                                        }
+
+data UIMenuItem = UIMenuItem { uiMenuItemEnabled :: Bool
+                             , uiMenuItemChecked :: Bool
+                             , uiMenuItemText    :: String
+                             }
+                | UIMenuItemQuit
+
+instance IsString UIMenuItem where
+    fromString s = UIMenuItem { uiMenuItemEnabled = True
+                              , uiMenuItemChecked = True
+                              , uiMenuItemText = s
+                              }
+
+data UIMenu = UIMenu { uiMenuName  :: String
+                     , uiMenuItems :: [UIMenuItem]
+                     }
 
 type VoidPtr = Ptr ()
 type CUIControl = VoidPtr
 
-foreign import ccall "uiMain"
+foreign import ccall safe "uiMain"
     c_uiMain :: IO ()
+
+foreign import ccall "uiQuit"
+    c_uiQuit :: IO ()
+
+foreign import ccall "uiUninit"
+    c_uiUninit :: IO ()
+
+foreign import ccall "uiInit"
+    c_uiInit :: Ptr CSize -> IO ()
+
+foreign import ccall "uiOnShouldQuit"
+    c_uiOnShouldQuit :: FunPtr (VoidPtr -> IO CInt) -> VoidPtr -> IO ()
 
 foreign import ccall "uiControlShow"
     c_uiControlShow :: CUIControl -> IO ()
+
+foreign import ccall "wrapper"
+    c_wrap1I :: (VoidPtr -> IO CInt) -> IO (FunPtr (VoidPtr -> IO CInt))
+
+foreign import ccall "wrapper"
+    c_wrap1 :: (VoidPtr -> IO ()) -> IO (FunPtr (VoidPtr -> IO ()))
 
 foreign import ccall "wrapper"
     c_wrap2 :: (VoidPtr -> VoidPtr -> IO ()) -> IO (FunPtr (VoidPtr -> VoidPtr -> IO ()))
@@ -128,7 +431,7 @@ foreign import ccall "uiCheckboxSetChecked"
     c_uiCheckboxSetChecked :: CUICheckbox -> CInt -> IO ()
 
 foreign import ccall "uiNewCheckbox"
-    c_uiNewCheckbox :: IO CUICheckbox
+    c_uiNewCheckbox :: CString -> IO CUICheckbox
 
 -- CUIEntry <- uiEntry
 type CUIEntry = VoidPtr
@@ -372,7 +675,7 @@ foreign import ccall "uiMenuAppendSeparator"
     c_uiMenuAppendSeparator :: CUIMenu -> IO ()
 
 foreign import ccall "uiNewMenu"
-    c_uiNewMenu :: IO CUIMenu
+    c_uiNewMenu :: CString -> IO CUIMenu
 
 -- UI alerts
 foreign import ccall "uiOpenFile"

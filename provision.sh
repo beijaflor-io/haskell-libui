1990315ff3212a694ed1c2281ab3de9af3843922
yamadapc <tacla.yamada@gmail.com>
2016-07-13 21:00:04 -0300

    Add vagrantfile

---
+++ provision.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install git -y
+git submodule update --init
+sudo apt-get update
+sudo apt-get install upstart upstart-job -y
+sudo apt-get install libgtk-3-dev -y
+wget -q -O- https://s3.amazonaws.com/download.fpcomplete.com/ubuntu/fpco.key | sudo apt-key add -
+echo 'deb http://download.fpcomplete.com/ubuntu/precise stable main'|sudo tee /etc/apt/sources.list.d/fpco.list
+sudo apt-get update && sudo apt-get install stack -y

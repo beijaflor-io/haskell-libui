b5cc6ee96c667d15972b361e11c807042c34da42
yamadapc <tacla.yamada@gmail.com>
2016-07-15 15:38:56 -0300

    submodule patch

---
+++ stuff.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.State
+
+type Stack   = [Int]
+type Output  = [Int]
+type Program = [Instr]
+
+type VM a = ReaderT Program (WriterT Output (State Stack)) a
+
+newtype Comp a = Comp { unComp :: VM a }
+  deriving (Functor, Applicative, Monad, MonadReader Program, MonadWriter Output, MonadState Stack)
+
+data Instr = Push Int | Pop | Puts
+
+evalInstr :: Instr -> Comp ()
+evalInstr instr = case instr of
+  Pop    -> modify tail
+  Push n -> modify (n:)
+  Puts   -> do
+    tos <- gets head
+    tell [tos]
+
+eval :: Comp ()
+eval = do
+  instr <- ask
+  case instr of
+    []     -> return ()
+    (i:is) -> evalInstr i >> local (const is) eval
+
+execVM :: Program -> Output
+execVM = flip evalState [] . execWriterT . runReaderT (unComp eval)
+
+program :: Program
+program = [
+     Push 42,
+     Push 27,
+     Puts,
+     Pop,
+     Puts,
+     Pop
+  ]
+
+main :: IO ()
+main = mapM_ print $ execVM program

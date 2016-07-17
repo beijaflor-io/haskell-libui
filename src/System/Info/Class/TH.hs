fb6d44d6879865acbb271f5ecd8267d4af9e4fd2
yamadapc <tacla.yamada@gmail.com>
2016-07-17 14:13:20 -0300

    Update TH idea

--- src/System/Info/Class/TH.hs
+++ src/System/Info/Class/TH.hs
@@ -5,17 +5,21 @@ module System.Info.Class.TH where
 import           Language.Haskell.TH
 import           System.Info
 
+-- | An empty class that defines which OS we're targetting
+--
+-- @
+-- fn :: TargetOS MacOS => IO ()
+-- fn = osxSpecificFunctionality
+-- @
 class TargetOS a where
 
 data MacOS
-
 data Linux
-
 data Windows
 
 makeOS :: Q [Dec]
 makeOS = case os of
     "darwin" -> [d| instance TargetOS MacOS where |]
-    "linux" -> [d| instance TargetOS MacOS where |]
-    "windows" -> [d| instance TargetOS Linux where |]
-    -- _ -> []
+    "linux" -> [d| instance TargetOS Linux where |]
+    "mingw32" -> [d| instance TargetOS Windows where |]
+    _ -> return []
